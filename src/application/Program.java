60cf7e6c61de0f2d8361f7def4a1f3269d29cd5a
Nailton Sousa <nailton.osn@gmail.com>
2023-04-16 20:31:47 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,9 +7,11 @@ import src.model.dao.SellerDao;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println("--- TEST 1: Seller findById ---");
         Seller seller = sellerDao.findById(3);
@@ -38,5 +40,12 @@ public class Program {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update completed!");
+
+        System.out.println("\n--- TEST 6: Seller delete ---");
+        System.out.println("Enter id for delete test:");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+        sc.close();
     }
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -10,7 +10,7 @@ public interface SellerDao {
 
     void update(Seller obj);
 
-    void deleteById(Seller id);
+    void deleteById(Integer id);
 
     Seller findById(Integer id);
 
